640504ff85b44b6bf4f91a6412bde10428118534
Andy Stabler <andy_60@hotmail.co.uk>
2015-05-06 17:17:40 +0100

    ADD sorter interface

---
+++ src/main/java/uk/co/andystabler/algorithms/sorting/Sorter.java
@@ -0,0 +1,11 @@
+package uk.co.andystabler.algorithms.sorting;
+
+import java.util.List;
+
+/**
+ * Created by Andy Stabler on 06/05/15.
+ */
+public interface Sorter<T> {
+
+    void sort(List<T> data);
+}
