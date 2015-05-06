5dbf26b7bf8abcfe07935727f8bc0e466866da77
Andy Stabler <andy_60@hotmail.co.uk>
2015-05-06 17:21:05 +0100

    ADD insertion sort tests

---
+++ src/test/java/uk/co/andystabler/algorithms/sorting/InsertionSortTest.java
@@ -0,0 +1,54 @@
+package uk.co.andystabler.algorithms.sorting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Andy Stabler on 06/05/15.
+ */
+public class InsertionSortTest {
+
+    @Test
+    public void emptyList_noChange() throws Exception {
+        List<String> data = null;
+        List<String> sorted = null;
+        SortingFactory.insertionSort(data);
+        Assert.assertEquals(sorted, data);
+    }
+
+    @Test
+    public void oneItem_noChange() throws Exception {
+        List<String> data = Collections.singletonList("Max");
+        List<String> sorted = Collections.singletonList("Max");
+        SortingFactory.insertionSort(data);
+        Assert.assertEquals(sorted, data);
+    }
+
+    @Test
+    public void TwoUnsortedStrings_sorted() throws Exception {
+        List<String> data = Arrays.asList("Max", "Barney");
+        List<String> sorted = Arrays.asList("Barney", "Max");
+        SortingFactory.insertionSort(data);
+        Assert.assertEquals(sorted, data);
+    }
+
+    @Test
+    public void TwoSortedStrings_noChange() throws Exception {
+        List<String> data = Arrays.asList("Barney", "Max");
+        List<String> sorted = Arrays.asList("Barney", "Max");
+        SortingFactory.insertionSort(data);
+        Assert.assertEquals(sorted, data);
+    }
+
+    @Test
+    public void TheeStrings_sorted() throws Exception {
+        List<String> data = Arrays.asList("Max", "Barney", "Andy", "Misty");
+        List<String> sorted = Arrays.asList("Andy", "Barney", "Max", "Misty");
+        SortingFactory.insertionSort(data);
+        Assert.assertEquals(sorted, data);
+    }
+}
