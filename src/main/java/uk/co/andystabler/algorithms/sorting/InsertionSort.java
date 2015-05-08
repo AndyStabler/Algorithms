c45893ee73bc24a96627161dd673b37d21da9675
Andy Stabler <andy_60@hotmail.co.uk>
2015-05-08 16:16:26 +0100

    UPDATE sorts to be more OOP

--- src/main/java/uk/co/andystabler/algorithms/sorting/InsertionSort.java
+++ src/main/java/uk/co/andystabler/algorithms/sorting/InsertionSort.java
@@ -7,17 +7,20 @@ import java.util.List;
  */
 public class InsertionSort<T extends Comparable<T>> implements Sorter<T> {
 
-    public void sort(List<T> data) {
+    private List<T> data;
+
+    public InsertionSort(List<T> data)
+    {
+        this.data = data;
+    }
+
+    public void sort() {
         if (data == null || data.size() < 2) return;
         System.out.println(data);
         for (int j = 1; j < data.size(); j++) {
             T key = data.get(j);
             // put data[key] into sorted list data[0 ... j-1]
             int i = j - 1;
-            System.out.println("j  : " + j);
-            System.out.println("key: " + key);
-            System.out.println("i  : " + i);
-            System.out.println(i + " > 0 ? " + (i > 0) + ". " + data.get(i) + " > " + key + "? " + (data.get(i).compareTo(key) > 0));
             while (i >= 0 && data.get(i).compareTo(key) > 0) {
                 data.set(i + 1, data.get(i));
                 i--;
--- src/main/java/uk/co/andystabler/algorithms/sorting/Sorter.java
+++ src/main/java/uk/co/andystabler/algorithms/sorting/Sorter.java
@@ -1,11 +1,9 @@
 package uk.co.andystabler.algorithms.sorting;
 
-import java.util.List;
-
 /**
  * Created by Andy Stabler on 06/05/15.
  */
 public interface Sorter<T> {
 
-    void sort(List<T> data);
+    void sort();
 }
