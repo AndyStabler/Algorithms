bd53d51e2ac852a0f90612e4e340e13bf1269e3c
Andy Stabler <andy_60@hotmail.co.uk>
2015-06-01 21:55:33 +0100

    ADD queue interface

---
+++ src/main/java/uk/co/andystabler/algorithms/datastructures/MyQueue.java
@@ -0,0 +1,15 @@
+package uk.co.andystabler.algorithms.datastructures;
+
+/**
+ * Created by Andy Stabler on 01/06/15.
+ */
+public interface MyQueue<T> {
+
+    boolean isEmpty();
+
+    T peek();
+
+    void offer(T data);
+
+    T poll();
+}
